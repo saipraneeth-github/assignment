a81c1ac6b0c89bd375ee8c6fe65c4e7f357c44e0
saipraneeth-github <57653037+saipraneeth-github@users.noreply.github.com>
2020-07-13 09:29:32 +0530

    Add files via upload

---
+++ Assignment-3/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function (window) {
+var byeSpeaker = { };
+var speakword ="good bye";
+byeSpeaker.speak=function (name) {
+	console.log(speakword +" "+ name);
+}
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ Assignment-3/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window) {
+var helloSpeaker={};
+var speakword ="Hello";
+helloSpeaker.speak=function(name){
+	console.log(speakword +" "+ name);
+}
+window.helloSpeaker = helloSpeaker;
+
+})(window);
